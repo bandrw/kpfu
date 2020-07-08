e80e13037b0a1d921b857201f4bdafe1f1652a02
Andreff16 <andr.b.2001@gmail.com>
2020-07-08 08:46:06 +0300

    first day final

--- 1_course/practice/project/js/main.js
+++ 1_course/practice/project/js/main.js
@@ -1,6 +1,9 @@
 function close_result()
 {
-	document.getElementById("result").remove();
+	document.getElementById("result").setAttribute("style", "transition: 0.25s; opacity: 0");
+	setTimeout(function(){
+		document.getElementById("result").remove();
+	}, 250);
 }
 
 function show_exit()
@@ -27,13 +30,90 @@ function input_handle()
 	return (data);
 }
 
-function get_books()
+function show_library(library)
+{
+	let i;
+
+	i = 0;
+	while (i < library.length)
+	{
+		div = document.createElement("div");
+		div.className = "library_book";
+		span = document.createElement("span");
+		span.className = "lib_book_author";
+		span.innerHTML = library[i].author;
+		div.appendChild(span);
+		span = document.createElement("span");
+		span.className = "lib_book_name";
+		span.innerHTML = library[i].name;
+		div.appendChild(span);
+		span = document.createElement("span");
+		span.className = "lib_book_country";
+		span.innerHTML = library[i].country;
+		div.appendChild(span);
+		span = document.createElement("span");
+		span.className = "lib_book_year";
+		span.innerHTML = library[i].year;
+		div.appendChild(span);
+		span = document.createElement("span");
+		span.className = "lib_book_count";
+		span.innerHTML = library[i].count;
+		div.appendChild(span);
+		document.getElementById("library_content").appendChild(div);
+		i++;
+	}
+}
+
+function get_library()
+{
+	let library;
+	let div;
+	let span;
+	let i;
+
+	library = [];
+	library[0] = {
+		author: "А.С. Пушкин",
+		name: "Дубровский",
+		country: "Russia",
+		year: 2000,
+		count: 10
+	};
+	library[1] = {
+		author: "Л.Н. Толстой",
+		name: "Война и мир",
+		country: "Russia",
+		year: 2000,
+		count: 10
+	};
+	library[2] = {
+		author: "Л.Н. Толстой",
+		name: "Война и мир",
+		country: "Russia",
+		year: 2000,
+		count: 10
+	};
+	library[3] = {
+		author: "Л.Н. Толстой",
+		name: "Война и мир",
+		country: "Russia",
+		year: 2000,
+		count: 10
+	};
+	library[4] = {
+		author: "Л.Н. Толстой",
+		name: "Война и мир",
+		country: "Russia",
+		year: 2000,
+		count: 10
+	};
+	return (library);
+}
+
+function books_cmp(data, lib)
 {
 	let books;
-	let data;
 
-	data = input_handle();
-	console.log(data);
 	books = [];
 	books[0] = {
 		author: "А.С. Пушкин",
@@ -50,6 +130,17 @@ function get_books()
 		count: 10
 	};
 	return (books);
+}
+
+function get_books()
+{
+	let lib;
+	let data;
+
+	data = input_handle();
+	console.log(data);
+	lib = get_library();
+	return (books_cmp(data, lib));
 }
 
 function show_result()
@@ -70,6 +161,7 @@ function show_result()
 		elem = document.createElement("ul");
 		elem.id = "books_list";
 		document.getElementById("result").appendChild(elem);
+		show_exit();
 	}
 	else
 		while (document.getElementById("books_list").firstChild)
@@ -88,7 +180,7 @@ function show_result()
 	}
 	else
 		document.getElementById("result_span").innerHTML = "Книги не найдены";
-	show_exit();
 }
 
+show_library(get_library());
 document.getElementById("lib_request_btn").onclick = show_result;
