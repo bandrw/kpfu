0846612e87c27a3f7b3cfeffbce2bf286029ed98
Andreff16 <andr.b.2001@gmail.com>
2020-07-10 14:05:33 +0300

    added sub-string search

--- 1_course/practice/project/js/main.js
+++ 1_course/practice/project/js/main.js
@@ -12,9 +12,37 @@ function get_input() // возвращает значения из инпуто�
 	return (data);
 }
 
+function str_next(str)
+{
+	let s;
+	let i;
+
+	s = [];
+	i = 1;
+	while (i < str.length)
+	{
+		s.push(str[i]);
+		i++;
+	}
+	return (s);
+}
+
+function ft_match(s1, s2)
+{
+	if (s1[0] == null && s2[0] == null)
+		return (1);
+	if (s1[0] == null && s2[0] == "*")
+		return (ft_match(s1, str_next(s2)));
+	if (s1[0] != null && s1[0] == s2[0])
+		return (ft_match (str_next(s1), str_next(s2)));
+	if (s1[0] != null && s2[0] == "*")
+		return (ft_match(s1, str_next(s2)) || ft_match (str_next(s1), s2));
+	return (0);
+}
+
 function match(str1, str2)
 {
-	if (str1 == "" || str1 == str2)
+	if (str1 == "" || ft_match(str2.toLowerCase(), ("*" + str1.toLowerCase() + "*")))
 		return (true);
 	else
 		return (false);
@@ -32,7 +60,7 @@ function get_books() // возвращает массив книг которы�
 	while (i < g_library.length)
 	{
 		if (match(data.author, g_library[i].author) && match(data.name, g_library[i].name) &&
-			match(data.country, g_library[i].country) && match(data.year, g_library[i].year) &&
+			match(data.country, g_library[i].country) && (data.year == "" || data.year == g_library[i].year) &&
 			data.count <= g_library[i].count)
 			res.push(g_library[i]);
 		i++;
