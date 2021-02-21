54afd934140a3fdd453f464a2bcb9feba29f5e2c
Andreff16 <andr.b.2001@gmail.com>
2021-02-21 23:09:22 +0300

    oib day01 ex01, ex02 added

---
+++ 2_course/oib/day01/ex01/main.py
@@ -0,0 +1,97 @@
+def is_alpha(ch):
+	return ord('a') <= ord(ch) <= ord('z') or ord('A') <= ord(ch) <= ord('Z')
+
+
+def decrypt_char(ch, shift):
+	upper = False
+	if ch.isupper():
+		upper = True
+		ch = chr(ord(ch) + ord('a') - ord('A'))
+	replacement = ord(ch) - shift
+	while replacement < ord('a'):
+		replacement += ord('z') - ord('a') + 1
+	if upper:
+		replacement -= (ord('a') - ord('A'))
+	return chr(replacement)
+
+
+def decrypt(text, shift):
+	res = ""
+	for ch in text:
+		if is_alpha(ch):
+			res += decrypt_char(ch, shift)
+		else:
+			res += ch
+	return res
+
+
+def encrypt_char(ch, shift):
+	upper = False
+	if ch.isupper():
+		upper = True
+		ch = chr(ord(ch) + ord('a') - ord('A'))
+	replacement = ord(ch) + shift
+	while replacement > ord('z'):
+		replacement -= ord('z') - ord('a') + 1
+	if upper:
+		replacement -= (ord('a') - ord('A'))
+	return chr(replacement)
+
+
+def encrypt(text, shift):
+	res = ""
+	for ch in text:
+		if is_alpha(ch):
+			res += encrypt_char(ch, shift)
+		else:
+			res += ch
+	return res
+
+
+def brute_force(text):
+	for i in range(0, 26):
+		print("[Shift: {}]\t\"{}\"".format(i, decrypt(text[0:20], i)))
+	print("Введите сдвиг: ")
+	shift = int(input("> "))
+	if shift <= 0:
+		raise Exception("invalid shift")
+	while shift >= 26:
+		shift -= 26
+	print("Result:\n{}".format(decrypt(text, shift)))
+
+
+def main():
+	shift = 0
+	print("Для шифрования введите 1, для расшифрования - 2, для перебора - 3")
+	mode = input("> ").strip()
+	if mode != "1" and mode != "2" and mode != "3":
+		raise Exception("invalid mode")
+	if mode != "3":
+		print("Введите сдвиг: ")
+		shift = int(input("> "))
+		if shift <= 0:
+			raise Exception("invalid shift")
+		while shift >= 26:
+			shift -= 26
+	print("Введите файл:")
+	file = open(input("> "), "r")
+	text = file.read()
+	file.close()
+	if mode == "1":
+		print("Result:\n{}".format(encrypt(text, shift)))
+	elif mode == "2":
+		print("Result:\n{}".format(decrypt(text, shift)))
+	else:
+		brute_force(text)
+
+
+if __name__ == "__main__":
+	while True:
+		try:
+			main()
+		except Exception as e:
+			print("Error: {}".format(e))
+		except KeyboardInterrupt:
+			print()
+			exit(0)
+		print()
---
+++ 2_course/oib/day01/ex02/main.py
@@ -0,0 +1,88 @@
+import random
+
+
+def generate_key(file_name):
+	file = open(file_name, "w")
+	alphabet = []
+	for i in range(ord('a'), ord('z') + 1):
+		alphabet.append(chr(i))
+	for i in range(ord('A'), ord('Z') + 1):
+		alphabet.append(chr(i))
+	shuffled = alphabet.copy()
+	random.shuffle(shuffled)
+	for i in range(0, 52):
+		file.write("{}:{}\n".format(alphabet[i], shuffled[i]))
+	file.close()
+	print("{} updated".format(file_name))
+
+
+def parse_key(file_name):
+	key = {}
+	file = open(file_name, "r")
+	lines = file.readlines()
+	file.close()
+	if len(lines) != 52:
+		raise Exception("Invalid file ({})".format(file_name))
+	for line in lines:
+		arr = line.strip("\n").split(":")
+		if len(arr) != 2:
+			raise Exception("Invalid file ({})".format(file_name))
+		key[arr[0]] = arr[1]
+	return key
+
+
+def encrypt(text, key):
+	res = ""
+	for ch in text:
+		tmp = key.get(ch)
+		if tmp is not None:
+			res += tmp
+		else:
+			res += ch
+	return res
+
+
+def decrypt(text, key):
+	decrypt_key = {}
+	for i in key:
+		decrypt_key[key.get(i)] = i
+	res = ""
+	for ch in text:
+		tmp = decrypt_key.get(ch)
+		if tmp is not None:
+			res += tmp
+		else:
+			res += ch
+	return res
+
+
+def main(file_name):
+	print("Для шифрования введите 1, для расшифрования - 2, для создания таблицы - 3")
+	mode = input("> ").strip()
+	if mode != "1" and mode != "2" and mode != "3":
+		raise Exception("invalid mode")
+	if mode == "1" or mode == "2":
+		print("Введите файл:")
+		file = open(input("> "), "r")
+		text = file.read()
+		file.close()
+		key = parse_key(file_name)
+		if mode == "1":
+			print("Result:\n{}".format(encrypt(text, key)))
+		else:
+			print("Result:\n{}".format(decrypt(text, key)))
+	else:
+		generate_key(file_name)
+
+
+if __name__ == "__main__":
+	key_file = "key.txt"
+	while True:
+		try:
+			main(key_file)
+		except KeyboardInterrupt:
+			print()
+			exit(0)
+		except Exception as e:
+			print("Error: {}".format(e))
+		print()
